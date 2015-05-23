27686918e0f7efa0aa6fe49e66390d0412ccdcc9
Erick Daniszewski <edaniszewski@gmail.com>
2015-05-23 11:09:35 -0400

    bump to v.02

--- colorutils/__init__.py
+++ colorutils/__init__.py
@@ -8,5 +8,5 @@ __description__ = "A utility for working with colors in Python."
 
 __copyright__ = "Copyright 2015"
 __license__ = "MIT"
-__version__ = "0.1"
+__version__ = "0.2"
 __status__ = "Development"
